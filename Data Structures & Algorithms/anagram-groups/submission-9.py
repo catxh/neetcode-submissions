fa852a7826f1081210576f1b545a73644a686594
Talon Haynes <haynestalon06@gmail.com>
2026-07-14 09:41:47 -0500

    Add: anagram-groups - submission-9

---
+++ Data Structures & Algorithms/anagram-groups/submission-9.py
@@ -0,0 +1,7 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = defaultdict(list)
+        
+        for str in strs:
+            anagrams[''.join(sorted(str))].append(str)
+        return list(anagrams.values())
